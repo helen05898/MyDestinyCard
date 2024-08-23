e8b452567e854349903c4fc23ac75643a51c14e4
helen05898 <helen05898@outlook.com>
2024-08-23 15:32:07 +0530

    Create src_components_Header.js

---
+++ src_components_Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="Header">
+      <h1>MyDestinyCard</h1>
+    </header>
+  );
+}
+
+export default Header;
