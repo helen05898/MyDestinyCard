586e997f0064052a8f09d2c7dba6ea3bed86a24b
helen05898 <helen05898@outlook.com>
2024-08-23 15:22:57 +0530

    Create ExpenseForm.js

---
+++ src_components/ExpenseForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './ExpenseForm.css';
+
+function ExpenseForm() {
+  return (
+    <div className="ExpenseForm">
+      <h2>Add New Expense</h2>
+      {/* Form content goes here */}
+    </div>
+  );
+}
+
+export default ExpenseForm;
