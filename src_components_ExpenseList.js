953a688205f25031c1342400c6fac3aeeec4f66e
helen05898 <helen05898@outlook.com>
2024-08-23 15:30:44 +0530

    Create src_components_ExpenseList.js

---
+++ src_components_ExpenseList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ExpenseList.css';
+
+function ExpenseList() {
+  return (
+    <ul className="ExpenseList">
+      {/* List items go here */}
+    </ul>
+  );
+}
+
+export default ExpenseList;
